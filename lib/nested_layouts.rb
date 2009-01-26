0e15e36931f1413d2c4da43522d2d902456e7b44
Nazar Aziz <nazar@panthersoftware.com>
2009-01-26 16:40:14 +0000

    nested-layout was not compatible with the theme_support plugin in Rails 2.1.2. Added an extra conditional to detect whether theme_support is installed. If so, then provide the theme's file path.

--- lib/nested_layouts.rb
+++ lib/nested_layouts.rb
@@ -2,7 +2,7 @@ module ActionView #:nodoc:
   module Helpers #:nodoc:
     module NestedLayoutsHelper
       # Binding is only required if we're running less than Rails 2.2
-      BINDING_REQUIRED = !!((Object.const_defined?(:Rails) && Rails.respond_to?(:version) ? 
+      BINDING_REQUIRED = !!((Object.const_defined?(:Rails) && Rails.respond_to?(:version) ?
           Rails.version : RAILS_GEM_VERSION) < '2.2.0')
 
       # Wrap part of the template into layout.
@@ -10,10 +10,21 @@ module ActionView #:nodoc:
       def inside_layout(layout, &block)
         binding = block.binding if BINDING_REQUIRED
 
-        layout = Dir.entries('app/views/layouts').detect { |a| /#{layout}/.match(a) }
+        #add support for theme_support plugin for Rails 2.1.2
+        if controller.respond_to?('current_theme') && !controller.current_theme.blank?
+          #the current theme may or may not override the requested layout. Check if it exists in the current theme
+          dir = File.join(Rails.root, 'themes', controller.current_theme, 'views', 'layouts')
+          #check if layout has been overridden in the theme... if not then default app/views/layouts
+          layout = Dir.entries(dir).detect { |a| /#{layout}/.match(a) }
+          dir = File.join(Rails.root, 'app', 'views', 'layouts') unless File.exists?(File.join(dir, layout))
+        else #theme support plugin not installed... continue...
+          dir = File.join(Rails.root, 'app', 'views', 'layouts')
+          layout = Dir.entries(dir).detect { |a| /#{layout}/.match(a) }
+        end
+
         @template.instance_variable_set('@content_for_layout', capture(&block))
         concat(
-          @template.render(:file => "#{RAILS_ROOT}/app/views/layouts/#{layout}", :user_full_path => true),
+          @template.render(:file => File.join(dir, layout), :user_full_path => true),
           binding
         )
       end
